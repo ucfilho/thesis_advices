11302ede3649b2bf20dbf9c9039a62c3d813d69a
ucfilho <42356381+ucfilho@users.noreply.github.com>
2018-12-12 10:11:56 -0200

    Delete ENX.py

--- ENX.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-
-def Enxame(PAR,NPAR,MAX,MIN):
-    x=np.zeros((NPAR, len(MAX)))
-    for j in range(len(MAX)):
-        for i in range(NPAR):
-            x[i,j]=MIN[j]+(MAX[j]-MIN[j])*np.random.random()
-    return x
